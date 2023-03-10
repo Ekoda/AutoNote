af7a26e680b660994d2c62cdb8d686c632191e25
Pontus Blomqvist <68470513+Ekoda@users.noreply.github.com>
2023-03-10 22:40:31 +0100

    Update Summarize.py

--- summarization/Summarize.py
+++ summarization/Summarize.py
@@ -8,10 +8,10 @@ def latest():
     time_str = time.strftime("%Y%m%d-%H%M%S")
     latest_file = max(glob.glob('trancription/files/*'), key=os.path.getctime)
     transcribed_text = open(latest_file, "r").read().strip()
-    maxLength = 5000
+    max_length = 5000
     meeting_notes = ""
-    if len(transcribed_text) > maxLength:
-        splitted_notes = [transcribed_text[i:i+maxLength] for i in range(0, len(transcribed_text), maxLength)]
+    if len(transcribed_text) > max_length:
+        splitted_notes = [transcribed_text[i:i+max_length] for i in range(0, len(transcribed_text), max_length)]
         summarized_notes = []
         for i, note in enumerate(splitted_notes):
             summarized_note = Api.summarize(note)
